f01b4ac2c0cddf5c22a7b90ad713934c884c6873
isaac <isaac@techvvs.io>
2021-09-09 11:50:03 -0500

    fixed syntax

--- collegeapp.sql
+++ collegeapp.sql
@@ -1,22 +1,22 @@
 
 
-CREATE TABLE student ( student_id INT NOT NULL
+CREATE TABLE student (student_id INT NOT NULL,
                          student_name VARCHAR (225),
                          birthday DATE,
-                            student_id INT
-);
-CREATE TABLE professor ( professor_id INT NOT NULL
+                        student_id INT)
+;
+CREATE TABLE professor ( professor_id INT NOT NULL,
                             professor_name VARCHAR(225),
                             hire_date DATE ,
-                            has_phd INT
-);
+                            has_phd INT)
+;
 
 CREATE TABLE building  (building_id INT NOT NULL,
                             building_name INT ,
                             date_built DATE,
                             coordinates VARCHAR,
                             capacity INT,
-                            PRIMARY KEY (building_id)
+                            PRIMARY KEY (building_id))
 );
 
 CREATE TABLE course (course_id INT NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE course (course_id INT NOT NULL,
                         creds INT,
                         description VARCHAR (225),
                         PRIMARY KEY (course_id),
-                        FOREIGN KEY (room_id) REFERENCES ROOM (room_id),
+                        FOREIGN KEY (room_id) REFERENCES room (room_id)),
 
 );
  CREATE TABLE book (book_id INT NOT NULL,
@@ -35,26 +35,26 @@ CREATE TABLE course (course_id INT NOT NULL,
                         price DOUBLE,
                         course_id INT,
                         PRIMARY KEY (book_id),
-                        FOREIGN KEY (course_id) REFERENCES course(course_id),
+                        FOREIGN KEY (course_id) REFERENCES course(course_id)),
 ;
 CREATE TABLE room (room_id INT NOT NULL,
                         capacity INT,
                         room_number INT,
                         building_id INT,
                         PRIMARY KEY (room_id),
-                        FOREIGN KEY (building_id) REFERENCES building(building_id)
+                        FOREIGN KEY (building_id) REFERENCES building(building_id))
 ;
  CREATE TABLE professorcourse ( professor_course_id INT NOT NULL,
                               professor_id INT,
                               course_id INT,
                               PRIMARY KEY (professor_course_id),
                               FOREIGN KEY (professor_id)REFERENCES professor(professor_id),
-                              FOREIGN KEY (course_id)REFERENCES course(course_id)
+                              FOREIGN KEY (course_id)REFERENCES course(course_id))
 ;
 CREATE TABLE studentcourse ( student_course_id INT NOT NULL,
                             course_id INT,
                             student_id INT,
                             PRIMARY KEY (student_course_id) ,
                             FOREIGN KEY (course_id)REFERENCES course(course_id),
-                            FOREIGN KEY (student_id)REFERENCES student(student_id)
+                            FOREIGN KEY (student_id)REFERENCES student(student_id))
 ;
